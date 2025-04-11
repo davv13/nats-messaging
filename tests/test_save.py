21781e0dbb30e2185bcb4da86c0b216ef4e95648
davitdavtyan969 <david.davtyan96@gmail.com>
2025-04-11 19:42:43 +0400

    Minor update

--- tests/test_save.py
+++ tests/test_save.py
@@ -1,3 +1,8 @@
+"""
+Test the save_message function from the message_repository module.
+This test checks if the function correctly saves messages to a file.
+"""
+
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
